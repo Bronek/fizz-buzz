81169e7645cc92ab181107813fc7bc4dbf33a306
Bronek Kozicki <brok@incorrekt.com>
2021-03-20 21:29:50 +0000

    more fixed_string
    https://godbolt.org/z/97x5WT

--- main.cpp
+++ main.cpp
@@ -1,81 +1,107 @@
 // vim: set ft=cpp et ts=4 sw=4:
 
-#include <array>
+#include <cstdio>
 #include <iostream>
+#include <array>
+#include <type_traits>
+#include <limits>
+
 
 template <size_t N>
 struct fixed_string {
-    std::array<char, N> contents;
+    // _data_ should be private
+    std::array<char, N> _data_;
 
+    constexpr fixed_string() {
+        for (size_t i = 0; i < N; ++i) {
+            _data_[i] = 0;
+        }
+    }
     constexpr fixed_string(char const (&str)[N]) {
         for (size_t i = 0; i < N; ++i) {
-            contents[i] = str[i];
+            _data_[i] = str[i];
         }
+        // always apply nul terminator
+        _data_[N - 1] = 0;
     }
     constexpr fixed_string(fixed_string const&) = default;
 
-    constexpr operator std::string_view() const {
-        return {contents.data(), N};
+    template <size_t Ns>
+    constexpr auto append(fixed_string<Ns> const& other) const -> fixed_string<N + Ns - 1>
+    {
+        auto res = fixed_string<N + Ns - 1>{};
+        for (size_t i = 0; i < N - 1; ++i) {
+            res._data_[i] = _data_[i];
+        }
+        for (size_t i = 0; i < Ns; ++i) {
+            res._data_[i + N - 1]  = other._data_[i];
+        }
+        return res;
     }
 
-    friend std::ostream& operator<<(std::ostream& o, const fixed_string& s) {
+    constexpr auto append(char c) const -> fixed_string<N + 1>
+    {
+        auto res = fixed_string<N + 1>{};
+        for (size_t i = 0; i < N - 1; ++i) {
+            res._data_[i] = _data_[i];
+        }
+        res._data_[N - 1] = c;
+        return res;
+    }
+
+    constexpr auto prepend(char c) const -> fixed_string<N + 1>
+    {
+        auto res = fixed_string<N + 1>{};
+        for (size_t i = 1; i <= N; ++i) {
+            res._data_[i] = _data_[i - 1];
+        }
+        res._data_[0] = c;
+        return res;
+    }
+
+    constexpr auto c_str() const -> char const* {
+        return _data_.data();
+    }
+
+    constexpr operator std::string_view() const {
+        return {_data_.data(), N};
+    }
+
+    friend auto operator<<(std::ostream& o, const fixed_string& s) -> std::ostream& {
         return (o << std::string_view{s});
     }
 };
 
-template <typename ...> struct data;
-
-template <typename T, typename S, typename ... Ts>
-struct data<T, S, Ts...> : data<Ts...> {
-    T element;
-
-    constexpr data(T&& v, data<Ts...>&& b)
-        : data<Ts...>(std::move(b))
-        , element(std::move(v))
-    {}
-
-    template <typename Sn>
-    constexpr auto const& get() const {
-        return this->select(std::type_identity_t<Sn>{});
+constexpr auto digits(unsigned int i) {
+    static_assert(sizeof(decltype(i)) == 4);
+    int res = 1;
+    while (i > 9) {
+        i /= 10;
+        res += 1;
     }
-
-    template <typename Sn, typename Tn>
-    constexpr auto append(Tn&& v) && {
-        return data<Tn, Sn, T, S, Ts...>{std::move(v), std::move(*this)};
-    }
-
-protected:
-    using data<Ts...>::select;
-    constexpr auto const& select(std::type_identity_t<S>) const {
-        return element;
-    }
-};
-
-template <>
-struct data<> {
-    constexpr data() = default;
-
-    template <typename Sn, typename Tn>
-    constexpr auto append(Tn&& v) && {
-        return data<Tn, Sn>{std::move(v), std::move(*this)};
-    }
-
-protected:
-    constexpr void select() const {}
-};
-
-template <auto v>
-void print() {
-    std::cout << v << '\n';
+    return res;
 }
 
-enum HelloWorld{};
-enum TheAnswer{};
+template <size_t N>
+constexpr auto format() -> fixed_string<digits(N) + 1> {
+    if constexpr (N > 9)
+        return format<(N / 10)>().append('0' + (N % 10));
+    else {
+        const char buf[2] = {'0' + N, 0};
+        return fixed_string{buf};
+    }
+}
+
+
 
 int main() {
-    constexpr auto d = data{}.
-        append<HelloWorld>(fixed_string{"HelloWorld"}).
-        append<TheAnswer>(42);
-    print<d.get<HelloWorld>()>();
-    print<d.get<TheAnswer>()>();
+    constexpr auto fizz = fixed_string{""}.append(fixed_string{"fizz"});
+    std::printf("%s\n", fizz.c_str());
+    constexpr auto buzz = fixed_string{"buzz"};
+    std::printf("%s\n", buzz.c_str());
+    constexpr auto fizzbuzz = fizz.append(buzz);
+    std::printf("%s\n", fizzbuzz.c_str());
+    std::cout << fizzbuzz.append(']').prepend('[') << '\n';
+    constexpr auto num = format<42>();
+    std::puts(fixed_string{"num:"}.append(num).c_str());
 }
